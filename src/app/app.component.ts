b112c7d2c12a359e2012a662332e857367451c1d
Luis Cabrera <gigocabrera@outlook.com>
2017-06-27 10:54:18 -0400

    fix(sound): replaced sound clip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,7 @@ export class CajaFuerteApp {
       Splashscreen.hide();
 
       // Enable audio
-      smartAudio.preload('tabSwitch', 'assets/audio/clickSound.wav');
+      smartAudio.preload('tabSwitch', 'assets/audio/clickSound.mp3');
 
       // Get local storage saved settings
       storage.ready().then(() => {
